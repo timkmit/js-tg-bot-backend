e03bf7b264484cb26c26efe202d49de21783cbd2
Timur <timengalytchev@mail.ru>
2023-04-04 01:12:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const token = '6258232797:AAHYOev0wX-Ka0kBYEY8vfnC_1nwOBonIOo';
+
+const TelegramBot = require('node-telegram-bot-api');
+const webAppUrl = 'https://js-tg-bot-8qqb.vercel.app/'
+
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if(text === '/start'){
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке снизу!',{
+        reply_markup:{
+            inline_keyboard:[
+                [{text:'Сделать заказ', web_app: {url:webAppUrl}}]
+            ]
+        }
+    });
+  }
+
+    
+});
